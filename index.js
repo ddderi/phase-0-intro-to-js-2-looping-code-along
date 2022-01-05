df79567882a67226a22648f463a26fcfb7e83cb9
ddderi <maxime830@live.fr>
2022-01-05 05:14:45 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me !`)
+   
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+
+function writeCards(arrayofstring, eventname){
+    let newarrayofstring = []
+    for (let i=0;i<arrayofstring.length;i++){
+       newarrayofstring.push(`Thank you, ${arrayofstring[i]}, for the wonderful ${eventname} gift!`)
+    }
+    return newarrayofstring;
+}
+
+// writeCards(['Guadalupe','Ollie','Aki'], 'surprise');
+
+function countDown(number){
+    for (let i=number;i>=0;i--){
+        console.log(i)
+    }
+}
+
